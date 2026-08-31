4021bb11fc1268a56bf03c68af67c9517e592b7a
home <artem.dolgopolov.314@gmail.com>
2026-08-31 20:07:30 +0300

    Initial commit

---
+++ src/client/java/tema/titanium/client/TitaniumModClient.java
@@ -0,0 +1,10 @@
+package tema.titanium.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TitaniumModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+        
+	}
+}
